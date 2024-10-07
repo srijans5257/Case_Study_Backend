9b350e864a39e50597898e23f2214ae78c433ec7
Srijan <srijans5257@gmail.com>
2024-10-07 11:12:08 +0530

    Initial Commit

---
+++ api/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+# Create your models here.
+class Project(models.Model):
+    name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Note(models.Model):
+    PENDING = 'pending'
+    ACCEPTED = 'accepted'
+    REJECTED = 'rejected'
+    
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (ACCEPTED, 'Accepted'),
+        (REJECTED, 'Rejected'),
+    ]
+
+    reason=models.CharField(max_length=100)
+    description=models.TextField()
+    created_at=models.DateField(auto_now_add=True)
+    from_date=models.DateField(default=created_at)
+    to_date=models.DateField(default=created_at)
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="notes")
+    status = models.CharField(
+        max_length=10, 
+        choices=STATUS_CHOICES, 
+        default=PENDING  # Default status is 'pending'
+    )
+
+    def __str__(self) -> str:
+        return self.reason
+
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ('manager', 'Manager'),
+        ('employee', 'Employee'),
+        ('admin','Admin'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname=models.CharField(max_length=255,blank=True,null=True)
+    lastname=models.CharField(max_length=255,blank=True,null=True)
+    email=models.EmailField(blank=True,null=True)
+    phone=models.IntegerField(blank=True,null=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+class Task(models.Model):
+    PENDING = 'pending'
+    ACCEPTED = 'accepted'
+    REJECTED = 'rejected'
+    
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (ACCEPTED, 'Accepted'),
+        (REJECTED, 'Rejected'),
+    ]
+    note = models.ForeignKey(Note, on_delete=models.CASCADE, related_name='tasks')
+    tasks_completed = models.TextField()
+    hours_requested=models.CharField(max_length=255, null=True,blank=True)
+    status = models.CharField(
+        max_length=10, 
+        choices=STATUS_CHOICES, 
+        default=PENDING  # Default status is 'pending'
+    )
+    date = models.DateField(null=True,blank=True)
+
+    def clean(self):
+        if self.date < self.note.from_date or self.date > self.note.to_date:
+            raise ValidationError('Date must be between the note\'s from_date and to_date.')
+    def __str__(self):
+        return f"Task for Note: {self.note.reason} for {self.date}"
+
+class Notifications(models.Model):
+    STATUS_CHOICES = [
+        ('Read', 'Read'),
+        ('Unread', 'Unread'),
+    ]
+    project=models.ForeignKey(Project,on_delete=models.SET_NULL,null=True, blank=True)
+    to=models.CharField(max_length=255)
+    from_manager=models.CharField(max_length=255)
+    status=models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='Read'
+    )
+    message=models.CharField(max_length=255,default="xyz")
---
+++ api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path("notes/",views.NoteListCreate.as_view(),name="note-list"),
+    path("notes_manager_pending/",views.NoteListCreateManagerPending.as_view(),name="note-list-manager"),
+    path("notes_manager_accepted/",views.NoteListCreateManagerAccepted.as_view(),name="note-list-manager"),
+    path("notes_manager_rejected/",views.NoteListCreateManagerRejected.as_view(),name="note-list-manager"),
+    path("notes/delete/<int:pk>/",views.NoteDelete.as_view(),name="delete-note"),
+    path("notes/<int:pk>/",views.NoteStatusUpdate.as_view(),name="update-note-status"),
+    path("taskaddition/<int:note_id>/<str:date>/",views.TaskCreateView.as_view(),name="task-addition"),
+    path('tasks/<int:pk>/', views.TaskDetailView.as_view(), name='task-detail'),
+    path('profile/<str:username>/', views.UserProfileView2.as_view(), name='user-profile'),
+    path('update-profile/<str:username>/', views.UpdateUserProfileView.as_view(), name='update-user-profile'),
+    path('tasks/',views.TaskView.as_view(),name='TaskView'),
+    path('get/allprofiles/',views.ProfileManagerView.as_view(),name="getallprofiles"),
+    path('get/allprofilesadmin/',views.ProfileAdminView.as_view(),name="getallprofiles"),
+    path('update/role/<str:username>/',views.roleChangeView.as_view(),name="change-role"),
+    path('get/notificationsemployee/',views.NotificationEmployeeView.as_view(),name="notification-employee"),
+    path('get/notificationsmanager/',views.NotificationManagerView.as_view(),name="notification-manager"),
+    path('post/notifications/',views.NotificationCreateView.as_view(),name="notification-create"),
+    path('update/notif/<int:pk>/',views.NotificationUpdateView.as_view(),name="notification-update"),
+]
